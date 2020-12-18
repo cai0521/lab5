ff66732800d83993186d90ef32a0953ac8bcd373
cai0521 <caild96@yahoo.com>
2020-12-18 16:33:17 +0300

    first commit

---
+++ src/bsu/rfe/java/lab5/group7/CAI/varA7/Mainframe.java
@@ -0,0 +1,91 @@
+package bsu.rfe.java.lab5.group7.CAI.varA7;
+
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+
+public class Mainframe extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private JFileChooser fileChooser = null;
+    private JMenuItem resetGraphicsMenuItem;
+    private GraphicsDisplay display = new GraphicsDisplay();
+    private boolean fileLoaded = false;
+
+    public Mainframe() {
+
+        super("Обработка событий от мыши");
+        this.setSize(700, 500);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        this.setLocation((kit.getScreenSize().width - 700) / 2, (kit.getScreenSize().height - 500) / 2);
+        this.setExtendedState(6);
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+        Action openGraphicsAction = new AbstractAction("Открыть файл с графиком") {
+            public void actionPerformed(ActionEvent event) {
+                if (Mainframe.this.fileChooser == null) {
+                    Mainframe.this.fileChooser = new JFileChooser();
+                    Mainframe.this.fileChooser.setCurrentDirectory(new File("."));
+                }
+
+                Mainframe.this.fileChooser.showOpenDialog(Mainframe.this);
+                Mainframe.this.openGraphics(Mainframe.this.fileChooser.getSelectedFile());
+            }
+        };
+        fileMenu.add(openGraphicsAction);
+        Action resetGraphicsAction = new AbstractAction("Отменить все изменения") {
+            public void actionPerformed(ActionEvent event) {
+                Mainframe.this.display.reset();
+            }
+        };
+        this.resetGraphicsMenuItem = fileMenu.add(resetGraphicsAction);
+        this.resetGraphicsMenuItem.setEnabled(false);
+        this.getContentPane().add(this.display, "Center");
+    }
+
+    protected void openGraphics(File selectedFile) {
+        try {
+            DataInputStream in = new DataInputStream(new FileInputStream(selectedFile));
+            ArrayList graphicsData = new ArrayList(50);
+
+            while(in.available() > 0) {
+                Double x = in.readDouble();
+                Double y = in.readDouble();
+                graphicsData.add(new Double[]{x, y});
+            }
+
+            if (graphicsData.size() > 0) {
+                this.fileLoaded = true;
+                this.resetGraphicsMenuItem.setEnabled(true);
+                this.display.displayGraphics(graphicsData);
+            }
+
+        } catch (FileNotFoundException var6) {
+            JOptionPane.showMessageDialog(this, "Указанный файл не найден", "Ошибка загрузки данных", 2);
+        } catch (IOException var7) {
+            JOptionPane.showMessageDialog(this, "Ошибка чтения координат точек из файла", "Ошибка загрузки данных", 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Mainframe frame = new Mainframe();
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
+    }
+}
